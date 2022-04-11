677f3ecb03a570d52518bcbacb36ac22cbf6d7d3
jk6236 <jk6236@nyu.edu>
2022-04-10 21:26:02 -0400

    Working on fscore

--- Corpus/test.py
+++ Corpus/test.py
@@ -14,7 +14,18 @@ def checkline():
     with open('Sample_Model_Output.txt') as f2:
         for line in f2:
             sample_output_list.append(line)
-    print(answers_list)
-    print(sample_output_list)
+    #print(answers_list)
+    #print(sample_output_list)
+    count = 0
+    for x in answers_list:
+        result = -1
+        base_word = answers_list[count]
+        result = base_word.find(sample_output_list[count])
+        if(result != -1):
+            print(result)
+        else:
+            print(result)
+        count = count + 1
+    
 
 checkline()
