af76ef81f3696475622c90d79d6f5145a9f26f2c
jyopari <play.jyo@gmail.com>
2022-04-26 10:19:01 -0400

    fixed eval script

--- evaluation/test.py
+++ evaluation/test.py
@@ -7,25 +7,27 @@ def checkline():
     sample_output_list = []
     with open('Answers.txt', "r", encoding = "utf-8") as f1:
         for line in f1:
-            answers_list.append(line)
-    with open('model_answers_dummy.txt', "r", encoding = "utf-8") as f2:
+            answers_list.append(line.strip('\n'))
+    with open('../model/model_answers_baseline_updated.txt', "r", encoding = "utf-8") as f2:
         for line in f2:
-            sample_output_list.append(line)
+            sample_output_list.append(line.strip('\n'))
     count = 0
     correct = 0
     
     print ("There are ", len(answers_list), "line in answer key, and ", len(sample_output_list), "line in model output")
+    
     for x in answers_list:
         result = -1
         base_word = answers_list[count]
         result = base_word.find(sample_output_list[count])
-        if(result != -1):
+        if(result == -1):
             count = count + 1
         else:
             correct = correct + 1
             count = count + 1
 
     accuracy = correct/count
+    print("correct ", correct, " total ", count)
     print("Accuracy: ", accuracy)
 
 checkline()
