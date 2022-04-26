a87058876222b6928edae644849fa3eec8910465
jyopari <play.jyo@gmail.com>
2022-04-25 22:53:39 -0400

    added debug lines to eval program

--- evaluation/test.py
+++ evaluation/test.py
@@ -8,11 +8,13 @@ def checkline():
     with open('Answers.txt', "r", encoding = "utf-8") as f1:
         for line in f1:
             answers_list.append(line)
-    with open('model_answers_baseline2.txt', "r", encoding = "utf-8") as f2:
+    with open('model_answers_dummy.txt', "r", encoding = "utf-8") as f2:
         for line in f2:
             sample_output_list.append(line)
     count = 0
     correct = 0
+    
+    print ("There are ", len(answers_list), "line in answer key, and ", len(sample_output_list), "line in model output")
     for x in answers_list:
         result = -1
         base_word = answers_list[count]
